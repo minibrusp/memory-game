b5fb167971098648a8cd6d6c212b763333b07cb3
XIII <minibrusp@gmail.com>
2023-01-28 01:06:46 +0800

    V.2 - Added Fun on level 5

--- game.js
+++ game.js
@@ -1,89 +1,112 @@
 var buttonColors = ["red", "blue", "green", "yellow"]
 var gamePattern = []
-var userClickedPattern = []
+var userChosenPattern = []
 var level = 0
 var started = false
 
+function nextSequence() {
+    level++
+    userChosenPattern = []
+    $("#level-title").text("Level "+ level)
+    var random = Math.floor(Math.random() * 4)
+    var randomChosenColor = buttonColors[random]
+    gamePattern.push(randomChosenColor)
+    makeSound(randomChosenColor)
+    $('#' + randomChosenColor ).fadeIn(100).fadeOut(100).fadeIn(100)
 
-
-$(".btn").click(function() {
-
-    var userChosenColor = $(this).attr("id")
-    userClickedPattern.push(userChosenColor)
-
-    playSound(userChosenColor)
-
-    animatePress(userChosenColor)
-
-    checkAnswer(userClickedPattern.length - 1)
-
-})
-
-
-$(document).keypress(function() {
-    if(!started) {
-        $("#level-title").text("Level "+ level)
-        nextSequence()
-        started = true
-    }
-})
-
-function animatePress(currentColor) {
-    $("#" + currentColor).addClass('pressed')
-    setTimeout(function() {
-        $("#" + currentColor).removeClass('pressed')
-    },100)
 }
 
-
-function nextSequence() {{
-    level++
-    userClickedPattern = []
-    $("#level-title").text("Level "+ level)
-    var randomNumber = Math.floor(Math.random() * 4)
-    var randomChosenColour = buttonColors[randomNumber]
-    gamePattern.push(randomChosenColour)
-
-    $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
-
-    playSound(randomChosenColour)
-
-}}
-
-function playSound(name) {
-    var audio = new Audio('./sounds/' + name + '.mp3')
+function makeSound(color) {
+    var audio = new Audio('./sounds/'+ color + '.mp3')
     audio.play()
 }
 
-function checkAnswer(currentLevel) {
-    if(userClickedPattern[currentLevel] === gamePattern[currentLevel]) {
+function animateButton(color) {
+    $('#' + color ).addClass('pressed')
 
-        if(userClickedPattern.length === gamePattern.length) {
-            setTimeout(function() {
-                nextSequence()
-            }, 1000)
+    setTimeout(function() {
+        $('#' + color ).removeClass('pressed')
+    }, 100)
+}
+
+function answerChecker(currentLevel) {
+    if(gamePattern[currentLevel] === userChosenPattern[currentLevel]) {
+
+        if(gamePattern.length === userChosenPattern.length) {
+
+            if(currentLevel === 4) {
+                var scareAudio = new Audio('./sounds/scream2.mp3')
+                // $('#scare-image').css("display", "block")
+                $('#scare-image').fadeToggle()
+                scareAudio.play()
+
+                setTimeout(function() {
+                    $('#scare-image').fadeToggle()
+                }, 2000)
+
+                setTimeout(function() {
+                    nextSequence()
+                }, 3000)
+
+            } else {
+                setTimeout(function() {
+                    nextSequence()
+                }, 1000)
+            }
+            
+            
         }
     } else {
+
         var audio = new Audio('./sounds/wrong.mp3')
         audio.play()
-        
+
         $("h1").text('Game Over, Press Any Key to Restart')
+
         $("body").addClass('game-over')
 
         setTimeout(function() {
             $("body").removeClass('game-over')
         }, 200)
-
-        startOver()
+        
+        reset()
     }
 }
 
-function startOver() {
-    level = 0
+function reset() {
     gamePattern = []
+    userChosenPattern = []
+    level = 0
     started = false
 }
 
 
 
+$('.btn').click(function() {
+    var chosenColor = $(this).attr("id")
+    makeSound(chosenColor)
+    animateButton(chosenColor)
 
+    userChosenPattern.push(chosenColor)
+
+    answerChecker(userChosenPattern.length - 1)
+
+})
+
+$(document).keypress(function(event) {
+    if(!started) {
+        nextSequence()
+        started = true
+    }
+})
+
+$(document).click(function(event) {
+
+    if(event.target == document.body) {
+        if(!started) {
+            nextSequence()
+            started = true
+        }
+    }
+
+})
